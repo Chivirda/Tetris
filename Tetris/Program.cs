4f0a858b3699489adf19fe1b937e4d3c0551a96c
Sergey Chivirda <si.chivirda@gmail.com>
2021-01-23 15:32:11 +0300

    feat: Draw 2 dots

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -6,7 +6,29 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            var x1 = 3;
+            var y1 = 2;
+            var c1 = '*';
+
+            Draw(x1, y1, c1);
+
+            var x2 = 8;
+            var y2 = 5;
+            var c2 = '#';
+
+            Draw(x2, y2, c2);
+
+
+            Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
         }
     }
 }
