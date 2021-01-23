db54b447c0a0813dea0fd2d7738ec2c58ab5e8c1
Sergey Chivirda <si.chivirda@gmail.com>
2021-01-23 15:55:06 +0300

    feat: add class Point

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -9,26 +9,13 @@ namespace Tetris
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            var x1 = 3;
-            var y1 = 2;
-            var c1 = '*';
-
-            Draw(x1, y1, c1);
-
-            var x2 = 8;
-            var y2 = 5;
-            var c2 = '#';
-
-            Draw(x2, y2, c2);
-
+            Point p1 = new Point();
+            p1.x = 4;
+            p1.y = 3;
+            p1.c = '*';
+            p1.Draw();
 
             Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char c)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(c);
         }
     }
 }
